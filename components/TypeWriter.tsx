ebbbbb537f4efd4470bd1dd248c8fa603732a11b
ZeezoLua <gwertxyt@gmail.com>
2024-09-22 15:20:12 +0300

    fixes x2

--- components/TypeWriter.tsx
+++ components/TypeWriter.tsx
@@ -1,30 +1,34 @@
-"use client"
+"use client";
 
-import React from 'react';
-import Typed from 'typed.js';
+import React from "react";
+import Typed from "typed.js";
 
-function TypeWriter( { text = "Example Text", speed = 50 } ) {
+function TypeWriter({ text = ["Example Text"], speed = 50 }) {
   // Create reference to store the DOM element containing the animation
   const el = React.useRef(null);
 
   React.useEffect(() => {
     const typed = new Typed(el.current, {
-      strings: [text], // Wrap text in an array
+      strings: text, // Using text as an array
       typeSpeed: speed,
       backSpeed: 50,
       backDelay: 600,
       loop: true,
+      smartBackspace: true, // Enable smart backspacing
+      showCursor: true,
+      cursorChar: "|",
+      contentType: "html", // This allows HTML tags like <b>
     });
 
     return () => {
       // Destroy Typed instance during cleanup to stop animation
       typed.destroy();
     };
-  }, []); // Add text and speed as dependencies
+  }, [text, speed]); // Add text and speed as dependencies
 
   return (
     <div className="App">
-      <span className='text-3xl drop-shadow-2xl' ref={el} />
+      <span className="text-3xl drop-shadow-2xl" ref={el} />
     </div>
   );
 }
