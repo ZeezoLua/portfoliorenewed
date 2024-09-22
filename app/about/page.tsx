90f41c36de5fbf9061173cfa84f96db108436b29
ZeezoLua <gwertxyt@gmail.com>
2024-09-22 15:30:17 +0300

    Responsive nowgit add --all

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -5,16 +5,18 @@ function About() {
     return (
         <>
             <Background />
-            <div className="w-screen h-[calc(100vh-78px)] flex justify-start items-center flex-col gap-12">
-                <h1 className="font-black text-6xl md:text-8xl text-[#FADFA1] drop-shadow-2xl mt-24 hover:scale-105 hover:-translate-y-1 transition-all">My Socials</h1>
-                <div className='flex justify-center items-center gap-4 text-4xl'>
-                    <a target='_blank' className='hover:underline decoration-wavy text-white hover:text-emerald-400 hover:scale-105 hover:-translate-y-1 transition-all duration-300' href="https://github.com/ZeezoLua">Github</a>
-                    -
-                    <a target='_blank' className='hover:underline decoration-wavy text-white hover:text-emerald-400 hover:scale-105 hover:-translate-y-1 transition-all duration-300' href="https://discord.com/users/719195873808220160">Discord</a>
-                    -
-                    <a target='_blank' className='hover:underline decoration-wavy text-white hover:text-emerald-400 hover:scale-105 hover:-translate-y-1 transition-all duration-300' href="https://www.tiktok.com/@ezz_xzy">Tiktok</a>
+            <main className="w-screen flex justify-start items-center h-[calc(100vh-72px)] flex-col gap-12 py-12">
+                <div className="flex justify-center items-center">
+                    <h1 className="font-black text-3xl md:text-8xl drop-shadow-2xl text-[#FADFA1] text-center hover:scale-105 hover:-translate-y-1 transition-all">My <br /> Socials</h1>
                 </div>
-            </div>
+                <div className="flex justify-center items-center rounded-full border-2 border-[#FADFA1] py-4 px-12 backdrop-blur-sm w-96 hover:scale-105 hover:-translate-y-1 gap-4 transition-all">
+                        <a target='_blank' className='hover:underline decoration-wavy text-white hover:text-emerald-400 hover:scale-105 hover:-translate-y-1 transition-all duration-300' href="https://github.com/ZeezoLua">Github</a>
+                        -
+                        <a target='_blank' className='hover:underline decoration-wavy text-white hover:text-emerald-400 hover:scale-105 hover:-translate-y-1 transition-all duration-300' href="https://discord.com/users/719195873808220160">Discord</a>
+                        -
+                        <a target='_blank' className='hover:underline decoration-wavy text-white hover:text-emerald-400 hover:scale-105 hover:-translate-y-1 transition-all duration-300' href="https://www.tiktok.com/@ezz_xzy">Tiktok</a>
+                </div>
+            </main>
         </>
     )
 }
--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -5,16 +5,18 @@ function About() {
     return (
         <>
             <Background />
-            <div className="w-screen h-[calc(100vh-78px)] flex justify-start items-center flex-col gap-12">
-                <h1 className="font-black text-6xl md:text-8xl text-[#FADFA1] drop-shadow-2xl mt-24 hover:scale-105 hover:-translate-y-1 transition-all">My Projects</h1>
-                <div className='flex justify-center items-center gap-4 text-4xl'>
+            <main className="w-screen flex justify-start items-center h-[calc(100vh-72px)] flex-col gap-12 py-12">
+                <div className="flex justify-center items-center">
+                    <h1 className="font-black text-3xl md:text-8xl drop-shadow-2xl text-[#FADFA1] text-center hover:scale-105 hover:-translate-y-1 transition-all">My <br /> Projects</h1>
+                </div>
+                <div className="flex justify-center items-center rounded-full border-2 border-[#FADFA1] py-4 px-12 backdrop-blur-sm w-96 hover:scale-105 hover:-translate-y-1 gap-4 transition-all">
                     <a target='_blank' className='hover:underline decoration-wavy text-white hover:text-emerald-400 hover:scale-105 hover:-translate-y-1 transition-all duration-300' href="https://zeezolua.github.io/AllCursors/">All Cursors</a>
                     -
                     <a target='_blank' className='hover:underline decoration-wavy text-white hover:text-emerald-400 hover:scale-105 hover:-translate-y-1 transition-all duration-300' href="https://zeezolua.github.io/Introduction/">Rizz</a>
                     -
                     <a target='_blank' className='hover:underline decoration-wavy text-white hover:text-emerald-400 hover:scale-105 hover:-translate-y-1 transition-all duration-300' href="https://zeezolua.github.io/DaisyUIProject/">DaisyUI</a>
                 </div>
-            </div>
+            </main>
         </>
     )
 }
